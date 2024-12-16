df0bec6b41b9e542d3869abb34eb523bb5c96d5e
2dam <2dam@TartangaLH.eus>
2024-12-16 10:43:27 +0100

    arreglos

---
+++ src/entities/Event.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 2dam
+ */
+@Entity
+@Table(name="event" , schema="nocturna")
+@XmlRootElement
+public class Event implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idEvent;
+    private String nombre;
+    private Date fecha;
+    private int NumEntradas;
+    private int consumicion;
+    private Double precioEntrada;
+    
+    
+    
+    public Long getId() {
+        return idEvent;
+    }
+
+    public void setId(Long idEvent) {
+        this.idEvent = idEvent;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getNumEntradas() {
+        return NumEntradas;
+    }
+
+    public void setNumEntradas(int NumEntradas) {
+        this.NumEntradas = NumEntradas;
+    }
+
+    public int getConsumicion() {
+        return consumicion;
+    }
+
+    public void setConsumicion(int consumicion) {
+        this.consumicion = consumicion;
+    }
+
+    public Double getPrecioEntrada() {
+        return precioEntrada;
+    }
+
+    public void setPrecioEntrada(Double precioEntrada) {
+        this.precioEntrada = precioEntrada;
+    }
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEvent != null ? idEvent.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Event)) {
+            return false;
+        }
+        Event other = (Event) object;
+        if ((this.idEvent == null && other.idEvent != null) || (this.idEvent != null && !this.idEvent.equals(other.idEvent))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "enties.EventEntity[ id=" + idEvent + " ]";
+    }
+    
+}
