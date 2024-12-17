d6ce1f7dcee0cf77719d7ec008d3ba92916d31b7
2dam <2dam@TartangaLH.eus>
2024-12-17 08:52:56 +0100

    commit entity de Event

--- src/entities/Event.java
+++ src/entities/Event.java
@@ -6,12 +6,21 @@
 package entities;
 
 import java.io.Serializable;
+import static java.sql.Date.valueOf;
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.validation.constraints.Future;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -24,23 +33,33 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Event implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long idEvent;
+    private Long id;
     private String nombre;
     private Date fecha;
     private int NumEntradas;
     private int consumicion;
     private Double precioEntrada;
+    private Club club; 
+    private Set artists;
+    private Set tickets;
+
+    public Event(String nombre, Date fecha, int NumEntradas, int consumicion, Double precioEntrada) {
+        this.nombre = "";
+        this.fecha = valueOf(LocalDate.now());
+        this.NumEntradas = 0;
+        this.consumicion = 0;
+        this.precioEntrada = 0.0;
+    }
+ 
     
-    
-    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     public Long getId() {
-        return idEvent;
+        return id;
     }
 
     public void setId(Long idEvent) {
-        this.idEvent = idEvent;
+        this.id = idEvent;
     }
 
     public String getNombre() {
@@ -50,7 +69,9 @@ public class Event implements Serializable {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Future
     public Date getFecha() {
         return fecha;
     }
@@ -84,11 +105,29 @@ public class Event implements Serializable {
     }
     
     
+    @ManyToOne
+    @JoinColumn(name="club")
+    public Club getClub(){
+       return  this.club;
+        
+    }
+    
+    @ManyToMany(mappedBy="events")
+    public Set getArtits(){
+       return  this.artists;
+        
+    }
+    
+    @OneToMany(mappedBy="ticket")
+    public Set getTickets(){
+       return  this.tickets;
+        
+    }
     
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (idEvent != null ? idEvent.hashCode() : 0);
+        hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
 
@@ -99,7 +138,7 @@ public class Event implements Serializable {
             return false;
         }
         Event other = (Event) object;
-        if ((this.idEvent == null && other.idEvent != null) || (this.idEvent != null && !this.idEvent.equals(other.idEvent))) {
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -107,7 +146,7 @@ public class Event implements Serializable {
 
     @Override
     public String toString() {
-        return "enties.EventEntity[ id=" + idEvent + " ]";
+        return "enties.EventEntity[ id=" + id + " ]";
     }
     
 }
