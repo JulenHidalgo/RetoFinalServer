034d0ed81ec03fb0965fea77b533367532af3606
2dam <2dam@TartangaLH.eus>
2025-01-10 11:46:41 +0100

    commit querys de user

--- src/service/UserFacadeREST.java
+++ src/service/UserFacadeREST.java
@@ -5,20 +5,29 @@
  */
 package service;
 
+import entities.Event;
 import entities.User;
+import java.sql.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javassist.NotFoundException;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 
 /**
  *
@@ -30,7 +39,8 @@ public class UserFacadeREST extends AbstractFacade<User> {
 
     @PersistenceContext(unitName = "NocturnaServerPU")
     private EntityManager em;
-
+    private static final Logger log = Logger.getLogger(ArtistFacadeREST.class.getName());
+    
     public UserFacadeREST() {
         super(User.class);
     }
@@ -82,7 +92,63 @@ public class UserFacadeREST extends AbstractFacade<User> {
     public String countREST() {
         return String.valueOf(super.count());
     }
-
+    
+    @GET
+    @Path("login/{mail}/{passwd}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public User login(@PathParam("mail") String mail, @PathParam("passwd") String passwd) throws NotFoundException {
+       User user=null;
+        Query query;
+        if(mail == null || passwd == null){
+            log.log(Level.INFO,"UserRESTful service: find users by event {0}.", mail);
+            throw new BadRequestException("Los parametros no pueden estar vacios");  
+        }
+        try {
+            log.log(Level.INFO,"UserRESTful service: find users by event {0}.", mail);
+            query=em.createNamedQuery("login");
+            query.setParameter("mail", mail);
+            query.setParameter("passwd", passwd);
+            user=(User) query.getSingleResult();
+            if(user==null)
+              throw new NotFoundException("El email o la contraseña no coinciden");
+            
+        }catch(NotFoundException e){
+            
+            throw new NotFoundException("El email o la contraseña no coinciden");
+            
+        } catch (Exception ex) {
+            log.log(Level.SEVERE,
+                    "ArtistRESTful service: Exception reading users by profile, {0}",
+                    ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+        return user;
+    }
+    
+    
+    
+    
+    @PUT
+    @Path("updatePasswd/{newPasswd}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void updatePasswd(@PathParam("newPasswd") String newPasswd, User user) {
+         Query query;
+         
+        if (newPasswd == null || newPasswd.isEmpty()) {
+            log.log(Level.INFO, "UserRESTful service: invalid new password {0}.", newPasswd);
+            throw new BadRequestException("Los parámetros no pueden estar vacíos");
+        }
+        try {
+            log.log(Level.INFO, "UserRESTful service: updating password for {0}.", newPasswd);
+            user.setPasswd(newPasswd);
+            super.edit(user);
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, "UserRESTful service: Exception updating password for {0}.", ex.getMessage());
+            throw new InternalServerErrorException(ex);
+        }
+    }
+ 
     @Override
     protected EntityManager getEntityManager() {
         return em;
